b61afd18e93c0d18a6aa73adab916b1b3dc06727
Salim Mayan <mailsalim@gmail.com>
2021-04-22 17:24:27 -0700

    Create two empty classes (Pastry.cs and Bread.cs)

---
+++ PierresBakery/Models/Bread.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace PierresBakery.Models
+{
+    public class Bread
+    {
+        // public string ConvertSentence(string source, string from, string to)
+        // {
+        //     string replacement = source.Replace(from, to);
+        //     return replacement;
+        // }
+    }
+}
--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,35 +1,35 @@
 using System;
-using FindReplace.Models;
+using PierresBakery.Models;
 using System.Collections.Generic;
 
-namespace FindReplace
+namespace PierresBakery
 {
     public class Program
     {
         public static void Main()
         {
-            int[] intArray = new int[5] { 10, 9, 13, 100, 1 };
+            // int[] intArray = new int[5] { 10, 9, 13, 100, 1 };
 
-            foreach (int eleBeforeSorting in intArray)
-            {
-                Console.WriteLine(eleBeforeSorting);
-            }
+            // foreach (int eleBeforeSorting in intArray)
+            // {
+            //     Console.WriteLine(eleBeforeSorting);
+            // }
 
-            Console.WriteLine("Array after sorting is ...");
-            Array.Sort(intArray);
+            // Console.WriteLine("Array after sorting is ...");
+            // Array.Sort(intArray);
 
-            foreach (int eleAfterSorting in intArray)
-            {
-                Console.WriteLine(eleAfterSorting);
-            }
-            // Console.WriteLine("Enter a sentence:");
-            // string source = Console.ReadLine();
-            // Console.WriteLine("Enter FROM string:");
-            // string from = Console.ReadLine();
-            // Console.WriteLine("Enter TO string:");
-            // string to = Console.ReadLine();
+            // foreach (int eleAfterSorting in intArray)
+            // {
+            //     Console.WriteLine(eleAfterSorting);
+            // }
+            // // Console.WriteLine("Enter a sentence:");
+            // // string source = Console.ReadLine();
+            // // Console.WriteLine("Enter FROM string:");
+            // // string from = Console.ReadLine();
+            // // Console.WriteLine("Enter TO string:");
+            // // string to = Console.ReadLine();
 
-            // FindR userSentence = new FindR();
+            // // FindR userSentence = new FindR();
 
             // string replacement = userSentence.ConvertSentence(source, from, to);
 
